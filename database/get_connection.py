fad742c717103ac1b6e16bfde89e0d0f49942c9f
thamarai kani <thamaraikani69@mail.com>
2022-04-07 18:04:40 +0530

    07-04-2022

--- database/get_connection.py
+++ database/get_connection.py
@@ -6,15 +6,19 @@ from sys import platform
 def get_connection():
 	if "linux" in platform:
 		connection = pymysql.connect(
-			host="Ip", user="user", password="password",
-			db="solar_panel", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor 
+			host="143.198.170.5", user="babu", password="Babux@1337",
+			db="solar_panel_tracking", charset="utf8", cursorclass=pymysql.cursors.DictCursor 
 			)
 	else:
-		
 		connection = pymysql.connect(
-			host="127.0.0.1", user="root", password="",
-			db="solar_panel", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor
+			host="143.198.170.5", user="babu", password="Babux@1337",
+			db="solar_panel_tracking", charset="utf8", cursorclass=pymysql.cursors.DictCursor 
 			)
+	# else:
+	# 	connection = pymysql.connect(
+	# 		host="127.0.0.1", user="root", password="",
+	# 		db="solar_panel", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor
+	# 		)
 	return connection
 
 
